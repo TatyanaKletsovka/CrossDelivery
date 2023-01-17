c3b532c00ac24bc1b1da447e4738f97f513a9662
t.kletsovka <t.kletsovka@syberry.com>
2023-01-17 16:48:06 +0300

    553340: Initialize a project with readme&docker files

---
+++ src/main/java/com/syberry/crossdelivery/CrossDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.syberry.crossdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossDeliveryApplication.class, args);
+	}
+
+}
